a25e8bb2463587c653425abbe0320c6cedc85570
isaacg <iwanyoike65@gmail.com>
2023-04-04 19:52:13 +0300

    Add booklist array

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,21 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [],
+    books: [
+        {
+            item_id: 'item1',
+            title: 'The Great Gatsby',
+            author: 'John Smith',
+            category: 'Fiction',
+        },
+        {
+            item_id: 'item2',
+            title: 'Anna Karenina',
+            author: 'Leo Tolstoy',
+            category: 'Fiction',
+        },
+        {
+            item_id: 'item3',
+            title: 'The Selfish Gene',
+            author: 'Richard Dawkins',
+            category: 'Nonfiction',
+        },
+    ],
 };
 
 const booksSlice = createSlice({
-  name: 'books',
-  initialState,
-  reducers: {
-    addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
-    removeBook: (state, action) => ({
-      ...state,
-      books: state.books.filter(
-        (book) => book.id !== action.payload,
-      ),
-    }),
-  },
+    name: 'books',
+    initialState,
+    reducers: {
+        addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
+        removeBook: (state, action) => ({
+            ...state,
+            books: state.books.filter(
+                (book) => book.item_id !== action.payload,
+            ),
+        }),
+    },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
