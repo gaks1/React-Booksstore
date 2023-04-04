855218bbb64b20f9f0198cd445298598776a2415
isaacg <iwanyoike65@gmail.com>
2023-04-04 16:37:59 +0300

    check linter errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    books: [],
+  books: [],
 };
 
 const booksSlice = createSlice({
-    name: 'books',
-    initialState,
-    reducers: {
-        addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
-        removeBook: (state, action) => ({
-            ...state,
-            books: state.books.filter(
-                (book) => book.id !== action.payload,
-            ),
-        }),
-    },
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
+    removeBook: (state, action) => ({
+      ...state,
+      books: state.books.filter(
+        (book) => book.id !== action.payload,
+      ),
+    }),
+  },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    categories: [],
-    status: 'Under construction',
+  categories: [],
+  status: 'Under construction',
 };
 
 const categoriesSlice = createSlice({
-    name: 'categories',
-    initialState,
-    reducers: {
-        getStatus: (state) => state.status,
-    },
+  name: 'categories',
+  initialState,
+  reducers: {
+    getStatus: (state) => state.status,
+  },
 });
 
 export const { getStatus } = categoriesSlice.actions;
