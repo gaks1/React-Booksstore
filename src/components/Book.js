abc092281e5695939149b467aa43efe1643dcada
isaacg <iwanyoike65@gmail.com>
2023-04-04 19:50:32 +0300

    Add addbook and removebook in book component and add handlesubmit function

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,30 +1,52 @@
-const Books = () => (
-  <div>
-    <ul>
-      <li>
-        Title: Book 1
-        Author: Author 1
-      </li>
-      <li>
-        Title: Book 2
-        Author: Author 2
-      </li>
-    </ul>
-    <h2>Add New Book</h2>
-    <form>
-      <label htmlFor="titleInput">
-        Title:
-        <input type="text" id="titleInput" name="title" />
-      </label>
-      <br />
-      <label htmlFor="authorInput">
-        Author:
-        <input type="text" id="authorInput" name="author" />
-      </label>
-      <br />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../redux/books/booksSlice';
+
+const Books = () => {
+  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = document.getElementById('titleInput').value;
+    const author = document.getElementById('authorInput').value;
+    const category = 'gtyu';
+    const id = Math.random().toString(36).substr(2, 9);
+    const book = {
+      item_id: id, title, author, category,
+    };
+    dispatch(addBook(book));
+  };
+
+  return (
+    <div>
+      <ul>
+        {
+          books.map((book) => (
+            <li key={book.item_id}>
+              <span>{book.title}</span>
+              <span>{book.author}</span>
+              <span>{book.category}</span>
+              <button type="button" onClick={() => dispatch(removeBook(book.item_id))}>Remove Book</button>
+            </li>
+          ))
+        }
+      </ul>
+      <h2>Add New Book</h2>
+      <form>
+        <label htmlFor="titleInput">
+          Title:
+          <input type="text" id="titleInput" name="title" />
+        </label>
+        <br />
+        <label htmlFor="authorInput">
+          Author:
+          <input type="text" id="authorInput" name="author" />
+        </label>
+        <br />
+        <button type="submit" onClick={(e) => handleSubmit(e)}>Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Books;
