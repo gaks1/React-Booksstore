aeafb413f55cfec5fe68ea72afca8784a0be3f17
isaacg <iwanyoike65@gmail.com>
2023-04-05 19:59:21 +0300

    Add urls and app id and fetchbooks

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,16 +1,41 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+const app_ID = '2ADQRdi3gXnFzDS1QgQh';
+
 const initialState = {
   books: [],
   isLoading: false,
   error: null,
 };
 
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(`${url}${app_ID}/books`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data);
+  }
+});
+
 const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchBooks.pending]: (state) => ({ ...state, isLoading: true }),
+    [fetchBooks.fulfilled]: (state, action) => ({
+      ...state,
+      books: action.payload,
+      isLoading: false,
+    }),
+    [fetchBooks.rejected]: (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }),
+  },
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
