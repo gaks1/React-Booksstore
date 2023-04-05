dbc0b3255e7ec8d98366610957ac18ffac908eb3
isaacg <iwanyoike65@gmail.com>
2023-04-05 19:51:41 +0300

    change initial state and reducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,40 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
+  isLoading: false,
+  error: null,
 };
 
 const booksSlice = createSlice({
   name: 'books',
   initialState,
-  reducers: {
-    addBook: (state, action) => ({ ...state, books: [...state.books, action.payload] }),
-    removeBook: (state, action) => ({
-      ...state,
-      books: state.books.filter(
-        (book) => book.item_id !== action.payload,
-      ),
-    }),
-  },
+  reducers: {},
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
